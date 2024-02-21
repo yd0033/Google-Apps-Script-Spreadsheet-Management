2f1cb194ed46ca137e2017571c491dd6eaf45135
yd0033 <yuuta.daitou.ry@s1.nttdocomo.com>
2024-02-21 16:01:21 +0900

    fix: Adjust code due to removal of column A in spreadsheet

--- src/adjustFontAndAlignment.js
+++ src/adjustFontAndAlignment.js
@@ -9,7 +9,6 @@ function adjustFontAndAlignment() {
   // 各関数を呼び出して範囲のプロパティを設定
   setFontProperties(range);
   setAlignment(sheet);
-  setRowBackgroundColor(sheet);
   setNumberFormat(sheet);
   setBorders(range);
 }
@@ -30,41 +29,24 @@ function setFontProperties(range) {
 // テキストの配置を設定する関数
 function setAlignment(sheet) {
   // ヘッダー行の範囲を取得し、中央揃えに設定
-  var headerRange = sheet.getRange("A1:G1");
+  var headerRange = sheet.getRange("A1:F1");
   headerRange.setHorizontalAlignment("center");
 
-  // A列の範囲を取得し、中央揃えに設定
-  var columnARange = sheet.getRange("A2:A" + sheet.getLastRow());
-  columnARange.setHorizontalAlignment("center");
+  // AからE列の範囲を取得し、左揃えに設定
+  var columnBtoERange = sheet.getRange("A2:E" + sheet.getLastRow());
+  columnBtoERange.setHorizontalAlignment("left");
 
-  // BからF列の範囲を取得し、左揃えに設定
-  var columnBtoFRange = sheet.getRange("B2:F" + sheet.getLastRow());
-  columnBtoFRange.setHorizontalAlignment("left");
-
-  // G列の範囲を取得し、右揃えに設定
-  var columnGRange = sheet.getRange("G2:G" + sheet.getLastRow());
-  columnGRange.setHorizontalAlignment("right");
-}
-
-// 行の背景色を設定する関数
-function setRowBackgroundColor(sheet) {
-  // A列の範囲を取得
-  var columnARange = sheet.getRange("A2:A" + sheet.getLastRow());
-  var values = columnARange.getValues();
-  // A列の値が0の行の背景色を灰色に設定
-  for (var i = 0; i < values.length; i++) {
-    if (values[i][0] == 0) {
-      sheet.getRange(i + 2, 1, 1, sheet.getLastColumn()).setBackground("#999999");
-    }
-  }
+  // F列の範囲を取得し、右揃えに設定
+  var columnFRange = sheet.getRange("F2:F" + sheet.getLastRow());
+  columnFRange.setHorizontalAlignment("right");
 }
 
 // 数値の形式を設定する関数
 function setNumberFormat(sheet) {
-  // G列の範囲を取得
-  var columnGRange = sheet.getRange("G2:G" + sheet.getLastRow());
-  // G列の数値の形式を6桁のゼロ埋めに設定
-  columnGRange.setNumberFormat("000000");
+  // F列の範囲を取得
+  var columnFRange = sheet.getRange("F2:F" + sheet.getLastRow());
+  // F列の数値の形式を6桁のゼロ埋めに設定
+  columnFRange.setNumberFormat("000000");
 }
 
 // 罫線を設定する関数
@@ -72,4 +54,3 @@ function setBorders(range) {
   // 範囲のセルに罫線を追加
   range.setBorder(true, true, true, true, true, true);
 }
-
